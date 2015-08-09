c5d8860b5764fe03c7124920a427b2d475d44a7d
linaljohnt <john@yeticode.co.uk>
2015-08-09 19:21:05 +0100

    added tsUnit

---
+++ ValidationPoc.UnitTests/scripts/App/QuestionnaireAppTests.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../tsunit/tsunit.ts" />
+/// <reference path="../../../ValidationPoc/scripts/Apps/QuestionnaireApp.ts" />
+module QuestionnaireAppTests {
+
+    export class MockControllerScope implements QuestionnaireApp.IControllerScope {
+        addPreviousName(): void {}
+
+        removePreviousName(number): void {}
+
+        data: QuestionnaireApp.QuestionnaireModel;
+    }
+
+    export class QuestionnaireControllerTests extends tsUnit.TestClass {
+        addPreviousNameAddsNewPreviousName() {
+
+            var scope = new MockControllerScope();
+            var controller = new QuestionnaireApp.QuestionnaireController(scope);
+            scope.addPreviousName();
+            this.areIdentical(1, scope.data.previousNames.length);
+        }
+    }
+}

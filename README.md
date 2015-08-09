7b66742e3a7c36b7468567ffb3f48a45640cf95c
linaljohnt <john@yeticode.co.uk>
2015-08-09 18:35:01 +0100

    Added unit test project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ValidationPOC
 
 This is a proof of concept of how to use `AngularJS` as client side validaiton with `FluentValidation` on the server side. `EntityFramework` is used to store the data using the CQRS pattern.
+
+The AngularJS app is implemented using `TypeScript`
---
+++ ValidationPoc.UnitTests/scripts/App/QuestionnaireAppTests.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../ValidationPoc/Scripts/angular.js"/>
+/// <reference path="../../ValidationPoc/Scripts/App/QuestionnaireApp.js"/>
+describe('QuestionnaireController', function () {
+    /*
+    beforeEach(module('QuestionnaireApp'));
+
+    var $controller;
+
+    beforeEach(inject(function (_$controller_) {
+        // The injector unwraps the underscores (_) from around the parameter names when matching
+        $controller = _$controller_;
+    }));
+    */
+    describe('$scope.addPreviousName', function () {
+        it('adds a previous name to the previous name collection', function () {
+            var $scope = {};
+
+            var controller = module('QuestionnaireApp').controller('QuestionnaireController', { $scope: $scope });
+            $scope.addPreviousName();
+            expect($scope.data.previousNames.length).toEqual(1);
+        });
+    });
+});
